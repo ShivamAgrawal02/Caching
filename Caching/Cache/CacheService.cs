da9b7ff59f555b1dfd17f74641377ab6488e5f03
shivamagrawal02 <shivamagrawal022001@gmail.com>
2026-06-13 09:54:13 +0530

    Added Generic Asynchronus Generic Caching service

---
+++ Caching/Cache/CacheService.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Caching.Cache
+{
+    public class CacheService : ICacheService
+    {
+        private IMemoryCache _memoryCache;
+        public CacheService(IMemoryCache memoryCache)
+        {
+           _memoryCache = memoryCache;
+        }
+        public Task<T?> GetAsync<T>(string key)
+        {
+            var data = _memoryCache.TryGetValue(key, out T value);
+            return Task.FromResult(value);
+        }
+
+        public Task RemoveAsync(string key)
+        {
+            _memoryCache.Remove(key);
+            return Task.CompletedTask;
+        }
+
+        public Task SetAsync<T>(string key, T Value, TimeSpan? expiration = null)
+        {
+            var options = new MemoryCacheEntryOptions();
+            if (expiration.HasValue)
+            {
+                options.SlidingExpiration = expiration.Value;
+            }
+            _memoryCache.Set(key, Value, options);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Caching/Cache/ICacheService.cs
@@ -0,0 +1,9 @@
+﻿namespace Caching.Cache
+{
+    public interface ICacheService
+    {
+        Task<T?> GetAsync<T> (string key);
+        Task SetAsync<T>(string key, T Value, TimeSpan? expiration=null);
+        Task RemoveAsync(string key);
+    }
+}
